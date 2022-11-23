0c55586186aba4b2c0be14a91b9a54ad548dda9d
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-23 13:01:36 +0100

    Created models

--- models/User.model.js
+++ models/User.model.js
@@ -3,6 +3,18 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
     email: {
       type: String,
       required: [true, 'Email is required.'],
@@ -10,10 +22,26 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true
     },
+    userImage: {
+      type: String,
+    },
     password: {
       type: String,
       required: [true, 'Password is required.']
-    }
+    },
+    aboutMe: {
+      type: String,
+    },
+    addresses: {type: [Schema.Types.ObjectId], ref: 'Addresses'},
+    createdItems: {type: [Schema.Types.ObjectId], ref: 'Items'},
+    borrowedItems: {type: [Schema.Types.ObjectId], ref: 'Items'},
+    messages: {type: [Schema.Types.ObjectId], ref: 'Messages'},
+    rating: {
+      type: String
+    },
+    friends: {
+      type: String
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
