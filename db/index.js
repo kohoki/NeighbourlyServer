a109546a5fbe613778fb07e426ad97793d17a7e5
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-24 11:42:15 +0100

    Integrated atlas mongo DB

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Neighbourly";
+  process.env.MONGODB_URI || "mongodb+srv://lisaphoeberalf:NzTgv9m9T1gOK5oO@neighbourly.94w5zfe.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI)
