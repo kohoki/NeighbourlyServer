c8bf4387aac888f6dc42092bb234981ce395ffd3
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-24 14:39:08 +0100

    add item routes

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use("/", authRouter);
 const profileRouter = require("./routes/profile.routes");
 app.use("/profile", profileRouter);
 
+const itemsRouter = require("./routes/item.routes");
+app.use("/item", itemsRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
