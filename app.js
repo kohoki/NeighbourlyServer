21ef4f306011aa02c5ba80ba7d5e99bf315644e1
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-30 17:34:55 +0100

    install npm core

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
